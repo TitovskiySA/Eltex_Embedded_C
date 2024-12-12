e05ae78ff44fd231e8511cb56d0031496f96a079
Stanislav <Titovsky_stas@mail.ru>
2024-12-12 13:18:56 +0700

    added task_6_2.c(nor ready)

--- task_6/task_6_2.c
+++ task_6/task_6_2.c
@@ -1,16 +1,60 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct somestr{
-	int num;
+#define MAXSTR = 100
+
+//struct somestr{
+//	int num;
+//	*char str;
+//};
+
+
+char *enter_dynamic_string(int *len){
+	printf("\nstart of enter_dyn\n");
+	*len = 0; // empty string
+	int len_mem = 1; //size of string with symbol "\0"
+	char *str = (char*)malloc(sizeof(char)); // creating pointer on our str
 	char symb;
-};
+	printf("\nscanf symb\n");
+	symb = getchar(); // enter some symbol
+	printf("\nend of getchar\n");
+
+	//end of string will be "\n"
+	while (symb != '\n'){
+		printf("\n--iter--\n");
+		str[(*len)++] = symb; //adding to string entered symbol, len is growing
+		len_mem = len_mem++; //so increasing size with symbol "\0"
+		str = (char*)realloc(str, len_mem * sizeof(char)); //increasing memory for string
+		symb = getchar(); //ask for new symbol
+	} 
+	
+	len_mem++;
+	str = (char*)realloc(str, len_mem * (sizeof(char)));
+	str[*len] = '\0'; // add end of string
+	return str; // return pointer on our string
+
+}
 
 void task(){
 	int MSize = 0;
 	printf("Enter size of massive\n");
 	scanf("%d", &MSize);
-	struct somestr *massive = (struct somestr*)calloc(MSize, sizeof(struct somestr));
+	
+	for (int i = 0; i < MSize; i++){
+		printf("\nEnter element #%d\n", i);
+		printf("Enter number of struct: ");
+		int num = 0;
+		scanf("%d", &num);
+		printf("\nEnter string of struct: ");
+		int len;
+		char *str = enter_dynamic_string(&len);
+		printf("you entered: %d, %s", num, str);
+		free(str); 
+
+	}
+
+
+	/*struct somestr *massive = (struct somestr*)calloc(MSize, sizeof(struct somestr));
 	if (massive == NULL){
 		printf("Error allocate memory\n");
 		return;
@@ -23,10 +67,10 @@ void task(){
 		*massive = item; 
 		printf("%d, %c \n", massive -> num, massive -> symb);
 		massive++;
-	}
+	}*/
 
 	printf("\n");
-	free(start_massive);
+//	free(start_massive);
 	return;
 }
 
