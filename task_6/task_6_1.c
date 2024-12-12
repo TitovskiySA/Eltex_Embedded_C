e85b9b091c6092c1bb1ce945ea86595458b82b2d
Stanislav <Titovsky_stas@mail.ru>
2024-12-12 10:13:55 +0700

    added task_6_1.c

---
+++ task_6/task_6_1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void task(){
+	int MSize = 0;
+	printf("Enter size of massive\n");
+	scanf("%d", &MSize);
+	int *massive = malloc(MSize * sizeof(int));
+	int *start_massive = massive;
+	for (int i = 0; i < MSize; i++){
+		*massive = i + 10;
+		printf("%d ", *massive);
+		massive++;
+	} 
+	printf("\n");
+	free(start_massive);
+	return;
+}
+
+int main(){
+	task();
+	return 0;
+}
