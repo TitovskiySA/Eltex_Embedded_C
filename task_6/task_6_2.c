2cc73652e9ff1fdcbcdd9e638b9a471db9b20ae9
TitovskiySA <Titovsiyystas@gmail.com>
2024-12-14 09:44:12 +0700

    Added task_6_2.c

--- task_6/task_6_2.c
+++ task_6/task_6_2.c
@@ -2,19 +2,18 @@
 #include <stdlib.h>
 
 struct some{
-	int num;
-	char** str;
+	int num; //here int
+	char** str; //here pointer on where dynamic string stores
 };
 
-/*char **/void enter_dynamic_string(char ** str_p/*int *len*/){
-        //printf("\nstart of enter_dyn\n");
+void enter_dynamic_string(char ** str_p){ //foo saves in this pointer address where entered dynamic string store
         char *str = (char*)malloc(sizeof(char)); // creating pointer on our str
-	//printf("created pointer on %p\n", str);
-        int symb;
-	//printf("ignoring symb %c = ", (char)symb);
-        str[0] = '\0'; // str must always have this at the end
-        int i = 0;
-	getchar(); // ignoring first symbol after scanf
+        int symb; // getchar returns int
+        str[0] = '\0'; // it must always have this at the end of string
+        int i = 0; //for cycle
+	while (getchar() == '\n'){//this cycle delete all \n symbols
+		//printf("\ndeleting some empty symbol");
+	}
         for (;;){
                 if (symb == '\n') break; // end of entered string
                 str = realloc(str, (i + 2) * sizeof(char)); // memory for our string + ending symbol
@@ -22,62 +21,53 @@ struct some{
                 str[i] = (char)symb; // writing our symbol in string
                 str[i+1] = '\0'; // adding finishing symbol
                 i++; // next cycle
-        }
-//        *len = i; // store of lengh of string   
-	*str_p = str;
-	printf("\n*str_p in foo = %p\n", str);
-	printf("string in p = %s\n", str);
+        }   
+	*str_p = str; // save to entered pointer address of result string
 	return;
-//        return str;
 }
-
-	
 
 void task(){
 	int mass_len = 0;
 	int size = sizeof(struct some);
 	struct some *massive = (struct some *)malloc(size);
-        struct some *massive_begin = massive;	
-	printf("pointer begin = %p\n", massive_begin);
-	int str_len = 0;
-	int num = 0;
-	char* some_p;
-	
-
+	if (massive == NULL){
+		printf("Error allocating memory for massive\n");
+		return;
+	}
+        struct some *massive_begin = massive;//stores begin of massive	
+	int num = 0;//for entering first element of struct
 	printf("Enter number of massive elements\n");
 	scanf("%d", &mass_len);
 	for (int i = 0; i < mass_len; i++){
 		printf("\nEnter .num for #%d item: ", i);
 		scanf("%d", &num);
-		//str_len = 0;
 		printf("\nEnter .str for #%d item: ", i);
-		char** str_p = (char **)malloc(sizeof(char));
-		printf("\nstr_p = %p\n", str_p);
-		enter_dynamic_string(str_p); 
-//		struct some item = {num, str_p};
-		printf("string at %p", str_p);
-//		massive = massive_begin + i;
-//		*massive = item;
-//		printf("\n struct #%d: %d %p", i, massive->num, massive->str);
-//		size = size + sizeof(struct some);
-		//printf("\n size = %d", size);
-//		massive_begin = realloc(massive_begin, size);
-//		printf("\nnow massive %p", massive);
-//		printf("\nnow mass_begin %p", massive_begin);
-//		massive = massive_begin + 1 + i;
+		char** str_p = (char **)malloc(sizeof(char));//allocate memory for second element of struct
+		if (str_p == NULL){
+			printf("Error allocating memory for str_p\n");
+			return;
+		}
+		enter_dynamic_string(str_p);//enter our string and save it addr to pointer str_p 
+		struct some item = {num, str_p};//creating element struct
+		massive = massive_begin + i;//iteration to next element of massive
+		*massive = item;//saving struct element to massive
+		size = size + sizeof(struct some);//increasing size of massive for next cycle
+		massive_begin = realloc(massive_begin, size);//reallocating memory for new size massive 
+		if (massive_begin == NULL){
+			printf("Error reallocating memory for massive_begin\n");
+			return;
+		}
 	
 	}
-//	massive = massive_begin;
-	//printf("all size = %d", size);
-	//printf("\nnow massive = %p", massive);
-	//printf("\nnow massive_begin = %p", massive_begin);
-//	for (int i = 0; i < mass_len; i++){
-//		printf("\nstruct #%d: %d, %p", i, massive->num, massive->str);
-//		massive++;
-//	}
+	massive = massive_begin;
+	for (int i = 0; i < mass_len; i++){
+		printf("\nstruct #%d: %d, %s", i, massive->num, *(massive->str));
+		free(massive->str); //free memory of string
+		massive++;
+	}
 	printf("\n");
 
-	free(massive_begin);
+	free(massive_begin); //free memory of massive
         return;	
 	}
 
