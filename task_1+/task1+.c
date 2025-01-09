04166e5b2e3d669563d1a5ab21515a19549edc9c
Stanislav <Titovsky_stas@mail.ru>
2025-01-09 14:11:23 +0700

    Changed task 1+.c

--- task_1+/task1+.c
+++ task_1+/task1+.c
@@ -8,23 +8,24 @@ int main(){
 	scanf("%u", &num);
 	printf("you entered num = %u\n", num);
 	number_of_1_bite(num);
+	return 0;
 }
 
 void number_of_1_bite(unsigned int n){
-	char result[sizeof(n)*32];
 	short quantity = 0;
 	for (size_t i = 0; i < sizeof(n) * 8; i++){
 		int res = n & (1 << i);
 		if (res > 0){
 			quantity++;
 			printf("1");
-			//result[31 - i] = "1";
 		}
-		else printf("0");//result[31 - i] = "0";
-		     
+		else printf("0");
 	}
-	//printf("result = %c\n", result);
-	printf("\nquantity = %d\n", quantity);
-	printf("result of task = %d", quantity % 2);
+	
+	if (quantity % 2 == 1){
+		printf("\nodd number of 1 (%d)\n", quantity);
+		return;}
+	printf("\neven number of 1 (%d)\n", quantity);
+	return;
 
 }
