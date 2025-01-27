321e1f0ebe07dbc79375c936c75aedf0cd3fcaea
TitovskiySA <Titovsiyystas@gmail.com>
2025-01-27 23:29:12 +0700

    second step to task_12

---
+++ task_12/task_12_client.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <sys/wait.h>           // for waitpid
+#include <unistd.h>             // for fork
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <signal.h>				// for signal handling
+#include <sys/socket.h>			// for sockets
+#include <fcntl.h>				// for fcntl foo
+#include <errno.h>				//for errno handler
+#include <sys/types.h>
+#include <netinet/in.h>			// for net settings
+#include <time.h>				// for nanosleep
+
+#define BUF_SIZE 1024 
+#define PORT 8080
+#define SERVER_ADDRESS "127.0.0.1"
+
+
+#define errExit(msg)    do { perror(msg); exit(EXIT_FAILURE); \
+                                   } while (0)
+void TCP_client();
+
+int main(){
+	TCP_client();
+	return 0;
+}
+
+void TCP_client(){
+	int sock_fd = 0;
+	struct sockaddr_in serv_addr;
+	char buffer[BUF_SIZE] = {0};
+	const char* clnt_mess = "Hello, I am client!";
+	
+	struct timespec t_sleep;
+	t_sleep.tv_sec = 1;
+	t_sleep.tv_nsec = 0;
+	
+	// creating client socket
+	if ((sock_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+		errExit("client sock create");
+	}
+	
+	// apply server address params
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(PORT);
+	
+	// store SERV_ADD to socket and check format
+	if (inet_pton(AF_INET, SERVER_ADDRESS, &serv_addr.sin_addr) <= 0){
+		errExit("client inet_pton");
+	}
+	
+	// set NON_BLOCKING mode to sock
+	int flags = fcntl(sock_fd, F_GETFL, 0);
+	if (fcntl(sock_fd, F_SETFL, flags | O_NONBLOCK) < 0){
+		close(sock_fd);
+		errExit("client fcntl");
+	}
+	
+	// connect to server
+	printf("start of connecting...\n");
+	if (connect(sock_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
+		//Check errno if connect in progress or it was failed
+		if (errno == EINPROGRESS){
+			printf("EINPROGRESS\n");
+		}else{
+			errExit("client connect error");
+		}
+	}
+	
+	// wait connection
+	nanosleep(&t_sleep, NULL);
+	
+	// send mess to serv
+	send(sock_fd, clnt_mess, strlen(clnt_mess), 0);
+	printf("Send to server: %s\n", clnt_mess);
+	
+	// read mess from serv
+	read(sock_fd, buffer, BUF_SIZE);
+	printf("server answered: %s\n", buffer);
+	
+	// close sock
+	close(sock_fd);
+	
+	return;
+}
--- task_12/task_12_server.c
+++ task_12/task_12_server.c
@@ -9,7 +9,8 @@
 #include <fcntl.h>				// for fcntl foo
 #include <errno.h>				//for errno handler
 #include <sys/types.h>
-#include <netinet/in.h>			// for?
+#include <netinet/in.h>			// for net settings
+#include <time.h>				// for nanosleep
 
 #define BUF_SIZE 1024 
 #define PORT 8080
@@ -22,31 +23,24 @@ void TCP_client();
 void TCP_server();
 
 int main(){
-	pid_t child;
-	// create process for client
-	if ((child = fork()) == -1){
-	    errExit( "child_1_pid fork" );
-	    }
-	    
-	//for child (client)
-	if ( child == 0 ) {
-		TCP_client();
-	}
-	else{
-		TCP_server();
-	}
+	TCP_server();
 	return 0;
 }
 
 void TCP_server(){
 	int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
-	int new_sock_fd = 0;
+	int new_sock_fd = 0, bytes_send = 0;
 	if (sock_fd < 0){
 		errExit("socket server failed");
 	}
 	
 	const char *hello_mess = "Hello from my TCP serv with NON_BLOCK socket";
-	char buffer[BUFF_SIZE] = {0};
+	char wr_buffer[BUF_SIZE] = {0};
+	char rd_buffer[BUF_SIZE] = {0};
+	
+	struct timespec t_sleep;
+	t_sleep.tv_sec = 1;
+	t_sleep.tv_nsec = 0;
 	
 	struct sockaddr_in serv_addr;
 	memset(&serv_addr, 0, sizeof(serv_addr));
@@ -61,7 +55,7 @@ void TCP_server(){
 	
 	//set server socket to non-block
 	int flags = fcntl(sock_fd, F_GETFL, 0);
-	if (fcntl(sock_fd, F_SETFL, flags | 0_NONBLOCK) < 0){
+	if (fcntl(sock_fd, F_SETFL, flags | O_NONBLOCK) < 0){
 		close(sock_fd);
 		errExit("server fcntl");
 	}
@@ -70,7 +64,7 @@ void TCP_server(){
 		close(sock_fd);
 		errExit("server listen");
 	}
-	printf("server listening jn port %d...\n", PORT);
+	printf("server listening on port %d...\n", PORT);
 	
 	//handling connections
 	while(1){
@@ -78,17 +72,39 @@ void TCP_server(){
 		socklen_t new_sock_len = sizeof(new_sock);
 		
 		//accept new conns
-		while((new_sock_fd = accept(sock_fd, (struct sockaddr*)&new_sock, (socketlen_t*)&new_sock_len)) >= 0){
+		while((new_sock_fd = accept(sock_fd, (struct sockaddr*)&new_sock, (socklen_t*)&new_sock_len)) >= 0){
 			printf("new conn, socket fd =%d\n", new_sock_fd);
+			
+			// read mess from clnt
+			read(new_sock_fd, rd_buffer, BUF_SIZE);
+			printf("server received: %s\n", rd_buffer);
 			
 			//send hello-mess to client
 			if ((bytes_send = send(new_sock_fd, hello_mess, strlen(hello_mess), 0)) < 0){
-				FINISHED HERE
+				perror("server send");
 			}
+			
+			// read mess from clnt
+			//read(new_sock_fd, rd_buffer, BUF_SIZE);
+			//printf("server received: %s\n", rd_buffer);
+						
+			close(new_sock_fd);
+			printf("server closed new_sock\n");
 		}
 		
+		// handling NON_BLOCK accept error
+		if (new_sock_fd < 0 && errno != EAGAIN && errno != EWOULDBLOCK){
+			perror("server accept failed");
+			break;
+		}
+		
+		//sleep for 1 sec
+		printf("server sleeps for 1 sec\n");
+		nanosleep(&t_sleep, NULL);
 	}
-	FINISHED HERE
+	
+	//close server sock
+	close(sock_fd);
 	
 	return;
 }
